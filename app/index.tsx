88d28ab06eeaeb41fd2a08f1590b2842dd36eaa4
KallosP <97070073+KallosP@users.noreply.github.com>
2024-07-02 23:29:24 -0700

    replaced modal with navigation, switched to expo router, cleaned up code

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Video } from 'expo-av';
-import /*MapView,*/ { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { router, useRouter } from "expo-router";
+import React, { useEffect, useState } from 'react';
+import { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import MapView from "react-native-map-clustering";
-
-
-import { StyleSheet, View, Modal, Image, Text, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const INITIAL_REGION = {
   latitude: 37.33,
@@ -62,28 +60,24 @@ type CCTV = {
     };
   }
 }
+
 // NOTE: don't rely on default component styles, will look different for every platform,
 //       ALWAYS make custom styles for otherwise default components (e.g. buttons)
-export default function App() {
+export default function HomeScreen() {
 
-  // Manages modal state (for when user clicks marker)
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const navigation = useRouter();
   // State fore storing all cctvs
   const [cams, setCams] = useState<CCTV[]>([]);
-  // State for tracking currently selected cctv (for displaying corresponding cctv image in modal)
-  const [selectedCCTV, setSelectedCCTV] = useState<CCTV>(); 
-  const [videoSource, setVideoSource] = useState<string>("");
-  const videoRef = useRef(null);
 
   /*
     TODO: 
-          - IMPROVE MODAL RENDER PERFORMANCE, takes too long to load (because of large cams array)
+          - IMPROVE PERFORMANCE OF LOADING PAGES, ADD LOADING ICONS INSTEAD OF JUST WAITING on empty screen
           - For live feeds that are in service but don't play, detect it somehow and show current image instead
             (go to fresno (districty 6), a lot of them are there; look into making the thumbnail of videos 
             default to current image? possible?)
-          - Decide on using modal or separate page for cam info
           - Decide on info to put under cam video/image
           - Implement highway search for highway conditions
+          - Add custom loading icon for app startup/app logo
           - Clean up UI
           - ...
   */
@@ -120,54 +114,24 @@ export default function App() {
       }
     };
 
-    // Fetch cctv data
-    /*fetch('https://cwwp2.dot.ca.gov/data/d1/cctv/cctvStatusD01.json')
-      // Convert response into json
-      .then((response) => response.json())
-      // Can access all properties of json object at this point aka all data related to cctv's
-      .then((json) => {
-        // Print data to console
-        //console.log(JSON.stringify(json.data, null, 2))
-
-        // Store all cameras from json object into CCTV array d1Cams
-        const d1Cams: CCTV[] = json.data.map((cam: CCTV) => cam);
-        // Set/update cams to hold all camera data
-        setCams(d1Cams)
-      })
-      // Catch any fetching error
-      .catch((error) => {
-        alert("Something went wrong...")
-        console.error(error)
-      })*/
-     fetchAllData();
+    fetchAllData();
   }, []) // Only fetch data once on app load
 
   // Custom handler for pressing a marker
   const handleMarkerPress = (cctv: CCTV) => {
-    // Set selectedCCTV to current cctv
-    setSelectedCCTV(cctv)
     // Set video source to current cctv
     var currCam = cctv.cctv.imageData.streamingVideoURL
-    if(currCam == ""){
-      //console.log("Video is not available for camera: ", cctv.cctv.index)
-      setVideoSource("")
-    }
-    else{
-      setVideoSource(currCam)
-    }
-    setIsModalVisible(true)
+    // Set img source to current cctv
+    var currImg = cctv.cctv.imageData.static.currentImageURL
+    // Pass current cctv info to cctv_details page
+    router.push({
+      pathname: "cctv_details",
+      params: {
+        videoSource: currCam,
+        imgSource: currImg
+      }
+    })
   }
-
-  const handleModalClose = () => {
-    setIsModalVisible(false)
-  }
-
-  /*const player = useVideoPlayer(videoSource, player => {
-    console.log('using player')
-    player.loop = true;
-    player.isLive = true;
-    player.play();
-  });*/
 
   return (
     <View style={styles.container}>
@@ -176,7 +140,7 @@ export default function App() {
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={INITIAL_REGION}
-        radius={110}
+        radius={140}
         minPoints={4}
         extent={812}
       // TODO: figure out why not displaying user location
@@ -194,59 +158,6 @@ export default function App() {
           />
         ))}
       </MapView>
-      
-
-      <Modal 
-        // Manage modal visibility with state
-        visible={isModalVisible}
-        // Dismisses modal when user presses back on Android or gestures on iOS
-        onRequestClose={handleModalClose}
-        // Animation of modal
-        animationType='fade'
-      >
-        {/* The layout of the modal */}
-        <View style={styles.modal}>
-          { // Only render the modal if there's a selected cctv/selcetedCCTV is not null (using boolean logic)
-          selectedCCTV && (
-            <View>
-              <>
-                {/*console.log(selectedCCTV.cctv.index)*/}
-              </>
-              
-              {videoSource !== "" ? (
-                  /* Display live video if available */
-                  <View>
-                   <Video
-                      ref={videoRef}
-                      source={{ uri: videoSource }}
-                      style={styles.video}
-                      // TODO: change later to custom controls?
-                      useNativeControls={true}
-                      shouldPlay={true}
-                      isLooping={true}
-                    />
-
-                    <Text>Displaying Video</Text>
-                  </View>
-                ) : (
-                  /* Display the selected cctv's image if video is not available */
-                  <View>
-                    <Image
-                      source={{ uri: selectedCCTV.cctv.imageData.static.currentImageURL}}
-                      style={styles.video}
-                    />
-                    <Text>No live video is available for this camera, displaying most recent image instead.</Text>
-                  </View>
-                )
-              
-              }
-              <Button title="Close" onPress={handleModalClose} />
-            
-            </View>
-          )}
-        </View>
-      </Modal>
-      
     </View>
   );
 }
@@ -256,11 +167,4 @@ const styles = StyleSheet.create({
   container: { flex: 1 },
   map: { width: '100%', height: '100%' },
   modal: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  video: {
-    width: 350,
-    height: 275,
-  },
-  controlsContainer: {
-    padding: 10,
-  },
 });
